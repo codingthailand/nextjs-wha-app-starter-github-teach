f4acb1cf09b0e7fde5adb36e84c719899edc705a
Akenarin Komkoon <codingthailand@gmail.com>
2026-06-11 01:50:55 +0700

    update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
           >
             <CirclePlay className="h-5! w-5!" /> ดูตัวอย่าง
                         <CirclePlay className="h-5! w-5!" /> ดูตัวอย่าง
-                                                <CirclePlay className="h-5! w-5!" /> ดูตัวอย่าง 11
+                                                <CirclePlay className="h-5! w-5!" /> ดูตัวอย่าง 12
 
           </Button>
         </div>
